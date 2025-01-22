9b31820d23ed4184a59cf35bb2f22bad261ca1c3
unknown <abhishek@ncompass.inc>
2025-01-22 12:54:16 +0530

    first commit

---
+++ lambda/index.js
@@ -0,0 +1,22 @@
+// lambda/index.js
+
+exports.handler = async (event) => {
+    const var1 = process.env.VAR1;
+    const var2 = process.env.VAR2;
+    const var3 = process.env.VAR3;
+  
+    console.log(`Environment Variable 1: ${var1}`);
+    console.log(`Environment Variable 2: ${var2}`);
+    console.log(`Environment Variable 3: ${var3}`);
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Lambda function executed successfully!',
+        var1,
+        var2,
+        var3,
+      }),
+    };
+  };
+  
