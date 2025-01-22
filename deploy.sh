23cfca8ba00f17b059b966d105885232c9ae4260
unknown <abhishek@ncompass.inc>
2025-01-22 14:16:57 +0530

    Added sh file

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Echo the CodeBuild version and resolved version (for debugging)
+echo "CodeBuild Source Version: $CODEBUILD_SOURCE_VERSION"
+echo "CodeBuild Resolved Source Version: $CODEBUILD_RESOLVED_SOURCE_VERSION"
+
+# Define environment variables
+VAR1=$VAR1
+VAR2=$VAR2
+VAR3=$VAR3
+
+# Create CloudFormation template.yaml with environment variables
+cat > template.yaml <<EOM
+AWSTemplateFormatVersion: '2010-09-09'
+Transform: AWS::Serverless-2016-10-31
+Resources:
+  abhishekLambdaCodebuild:
+    Type: AWS::Serverless::Function
+    Properties:
+      FunctionName: abhishek-codebuild-${VAR1}
+      Handler: index.handler
+      Runtime: nodejs16.x
+      AutoPublishAlias: default
+      Timeout: 30
+      DeploymentPreference:
+        Enabled: True
+      Environment:
+        Variables:
+          VAR1: $VAR1
+          VAR2: $VAR2
+          VAR3: $VAR3
+EOM
+
+# Deploy using AWS CloudFormation
+echo "Deploying CloudFormation stack..."
+aws cloudformation deploy \
+  --template-file template.yaml \
+  --stack-name abhishek-codebuild-${VAR1} \
+  --capabilities CAPABILITY_IAM
+
+# Optional: Check the stack status
+aws cloudformation describe-stacks --stack-name abhishek-codebuild-${VAR1}
