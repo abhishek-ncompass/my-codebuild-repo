7342eb4fb9a07298178128fe0d7e1a76f9afd71c
unknown <abhishek@ncompass.inc>
2025-01-23 11:25:01 +0530

    fixing the artifact file in buildspec file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,6 @@
-# Echo the CodeBuild version and resolved version (for debugging)
-# echo "CodeBuild Source Version: $CODEBUILD_SOURCE_VERSION"
-# echo "CodeBuild Resolved Source Version: $CODEBUILD_RESOLVED_SOURCE_VERSION"
+echo "CodeBuild Source Version: $CODEBUILD_SOURCE_VERSION"
+echo "CodeBuild Resolved Source Version: $CODEBUILD_RESOLVED_SOURCE_VERSION"
 
-# Create CloudFormation template.yaml with environment variables
 cat > template.yaml <<EOM
 AWSTemplateFormatVersion: '2010-09-09'
 Transform: AWS::Serverless-2016-10-31
@@ -22,12 +20,4 @@ Resources:
           VAR4: $VAR4
 EOM
 
-# # Deploy using AWS CloudFormation
-# echo "Deploying CloudFormation stack..."
-# aws cloudformation deploy \
-#   --template-file template.yaml \
-#   --stack-name abhishek-codebuild-${VAR1} \
-#   --capabilities CAPABILITY_IAM
-
-# # Optional: Check the stack status
-# aws cloudformation describe-stacks --stack-name abhishek-codebuild-${VAR1}
+cat template.yaml
